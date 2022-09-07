565a5335b490f2007c94df4ab98d9acbfb2c689f
a9sDanielJaeger <djaeger@anynines.com>
2022-09-07 08:13:51 +0200

    now handling input errors and improved searching for key-values

--- main.rb
+++ main.rb
@@ -6,13 +6,13 @@ set :port, 4567
 
 #Going to be killed for having a password written right here
 $database_password = 'AvyQji-VR-9Ndwh'
-$host = 'localhost'
-$port = '5432'
+$database_host = 'localhost'
+$database_port = '5432'
 $database_name = 'ProgrammingLanguages'
 $database_user = 'postgres'
 
 get '/get-all' do
-    connection = PG.connect(:host => $host, :port => $port, :dbname => $database_name, :user => $database_user, :password => $database_password)
+    connection = PG.connect(:host => $database_host, :port => $database_port, :dbname => $database_name, :user => $database_user, :password => $database_password)
 
     entries = connection.exec "SELECT * FROM \"progLanguages\".languages"
 
@@ -26,13 +26,13 @@ get '/get-all' do
 end
 
 get '/get' do
-    connection = PG.connect(:host => $host, :port => $port, :dbname => $database_name, :user => $database_user, :password => $database_password)
+    connection = PG.connect(:host => $database_host, :port => $database_port, :dbname => $database_name, :user => $database_user, :password => $database_password)
 
-    value_to_look_for = params["name"]
-    entry = connection.exec "SELECT * FROM \"progLanguages\".languages WHERE name = \'#{value_to_look_for}\'"
+    name_to_look_for = params["name"].downcase
+    entry = connection.exec "SELECT * FROM \"progLanguages\".languages WHERE lower(name) = \'#{name_to_look_for}\'"
     
     if entry.values.length == 0
-        return "Could not find #{value_to_look_for} in database"
+        return "Could not find #{name_to_look_for} in database"
     end
 
     entry.map do |value|
@@ -45,7 +45,19 @@ post '/add' do
     creation_date = params["creation-date"]
     founder = params["founder"]
 
-    connection = PG.connect(:host => $host, :port => $port, :dbname => $database_name, :user => $database_user, :password => $database_password)
+    if !creation_date.match(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/)
+        return "The given creation date is not in the format yyyy-mm-dd!"
+    end
+
+    if !name.match(/^[a-zA-z\s-]{1,20}$/)
+        return "Names must not contain characters other than letters, spaces and dashes(-) and can have 20 characters at most"
+    end
+
+    if !founder.match(/^[a-zA-z\s-]{1,20}$/)
+        return "Founders must not contain characters other than letters, spaces and dashes(-) and can have 20 characters at most"
+    end
+
+    connection = PG.connect(:host => $database_host, :port => $database_port, :dbname => $database_name, :user => $database_user, :password => $database_password)
 
     result = connection.exec "SELECT * FROM \"progLanguages\".languages WHERE name = \'#{name}\'"
 
@@ -57,10 +69,11 @@ post '/add' do
         name, creation_date, founder)
         VALUES ('#{name}', '#{creation_date}', '#{founder}');"
     
-    if result.result_status == 1
-        "Successfully added entry in table!"
-    else
-        "#Ups! Something went wrong."
-    end
+    #if result.result_status == 1
+    #    "Successfully added entry in table!"
+    #else
+    #    "#Ups! Something went wrong."
+    #end
 
+    result.res_status(result.result_status)
 end
