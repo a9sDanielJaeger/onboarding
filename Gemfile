7b7d18bb01118fe61429a3f6e8a33c6e38cbeb10
a9sDanielJaeger <djaeger@anynines.com>
2022-09-07 09:26:42 +0200

    added config and gemfile. added delete-endpoint

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "http://rubygems.org"
+gem "sinatra", "~> 2.2"
+gem "pg", "~> 1.4"
+gem "yaml", "~> 0.2.0"
--- main.rb
+++ main.rb
@@ -1,44 +1,35 @@
 require 'sinatra'
 require 'pg'
+require 'yaml'
+
+$config = YAML.load_file('config/config.yml')['database']
 
 set :bind, '0.0.0.0'
 set :port, 4567
 
-#Going to be killed for having a password written right here
-$database_password = 'AvyQji-VR-9Ndwh'
-$database_host = 'localhost'
-$database_port = '5432'
-$database_name = 'ProgrammingLanguages'
-$database_user = 'postgres'
+def pg_client
+  PG.connect(:host => $config['host'], :port => $config['port'], :dbname => $config['name'], :user => $config['user'], :password => $config['password'])
+end
 
 get '/get-all' do
-    connection = PG.connect(:host => $database_host, :port => $database_port, :dbname => $database_name, :user => $database_user, :password => $database_password)
+    entries = pg_client.exec "SELECT * FROM \"progLanguages\".languages"
 
-    entries = connection.exec "SELECT * FROM \"progLanguages\".languages"
-
-    if entries.result_status == 0
-        return "There is no entry"
-    end
-
+    return "There is no entry" if entries.result_status == 0
+   
     entries.map do |entry|
         JSON[entry] + "\n"
     end
 end
 
 get '/get' do
-    connection = PG.connect(:host => $database_host, :port => $database_port, :dbname => $database_name, :user => $database_user, :password => $database_password)
 
     name_to_look_for = params["name"].downcase
 
-    if !name_to_look_for.match(/^[a-z\s-]{1,20}$/)
-        return "Names can only contain letters, spaces and dashes and must not be longer than 20 characters!"
-    end
+    return "Names can only contain letters, spaces and dashes and must not be longer than 20 characters!" unless name_to_look_for.match(/^[a-z\s-]{1,20}$/)
     
-    entry = connection.exec "SELECT * FROM \"progLanguages\".languages WHERE lower(name) = \'#{name_to_look_for}\'"
+    entry = pg_client.exec "SELECT * FROM \"progLanguages\".languages WHERE lower(name) = \'#{name_to_look_for}\'"
     
-    if entry.values.length == 0
-        return "Could not find #{name_to_look_for} in database"
-    end
+    return "Could not find #{name_to_look_for} in database" if entry.values.length == 0
 
     entry.map do |value|
         JSON[value]
@@ -50,35 +41,30 @@ post '/add' do
     creation_date = params["creation-date"]
     founder = params["founder"]
 
-    if !creation_date.match(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/)
-        return "The given creation date is not in the format yyyy-mm-dd!"
-    end
+    return "The given creation date is not in the format yyyy-mm-dd!" unless creation_date.match(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/)
 
-    if !name.match(/^[a-zA-Z\s-]{1,20}$/)
-        return "Names must not contain characters other than letters, spaces and dashes(-) and can have 20 characters at most"
-    end
+    return "Names must not contain characters other than letters, spaces and dashes(-) and can have 20 characters at most" unless name.match(/^[a-zA-Z\s-]{1,20}$/)
 
-    if !founder.match(/^[a-zA-Z\s-]{1,20}$/)
-        return "Founders must not contain characters other than letters, spaces and dashes(-) and can have 20 characters at most"
-    end
+    return "Founders must not contain characters other than letters, spaces and dashes(-) and can have 20 characters at most" unless founder.match(/^[a-zA-Z\s-]{1,20}$/)
 
-    connection = PG.connect(:host => $database_host, :port => $database_port, :dbname => $database_name, :user => $database_user, :password => $database_password)
+    connection = pg_client
 
     result = connection.exec "SELECT * FROM \"progLanguages\".languages WHERE name = \'#{name}\'"
 
-    if result.values.length > 0
-        return "Programming language already exists!"
-    end
+    return "Programming language already exists!" if result.values.length > 0
 
     result = connection.exec "INSERT INTO \"progLanguages\".languages(
         name, creation_date, founder)
         VALUES ('#{name}', '#{creation_date}', '#{founder}');"
     
-    #if result.result_status == 1
-    #    "Successfully added entry in table!"
-    #else
-    #    "#Ups! Something went wrong."
-    #end
 
     result.res_status(result.result_status)
+end
+
+post '/delete' do
+  name = params["name"]
+
+  result = pg_client.exec "DELETE FROM \"progLanguages\".languages WHERE lower(name) = \'#{name}\'"
+
+  result.res_status(result.result_status)
 end
